cdadce44b0cb6433175005e9ef6a7770a835b0a5
Rahul  Kumar <121877673+rahul399517@users.noreply.github.com>
2023-06-04 00:46:21 -0700

    Test Case updated

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -2,8 +2,8 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "../App";
 
-test("renders 'working....' text", () => {
+test("renders 'Testcase added' text", () => {
   render(<App />);
-  const workingTextElement = screen.getByText("working....");
+  const workingTextElement = screen.getByText("Testcase added");
   expect(workingTextElement).toBeInTheDocument();
 });
